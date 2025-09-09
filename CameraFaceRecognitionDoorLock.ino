af4983496ca75d643943cb2622bdeab53e40b461
Naeem Alawity <naem.mohe.ps@gmail.com>
2025-09-09 10:51:20 +0300

    رسالة التغيير

--- CameraFaceRecognitionDoorLock.ino
+++ CameraFaceRecognitionDoorLock.ino
@@ -1,109 +1,96 @@
-
 #include "esp_camera.h"
 #include <WiFi.h>
 
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
-
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_ESP_EYE
-//#define CAMERA_MODEL_M5STACK_PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE
+// ===== Camera model =====
 #define CAMERA_MODEL_AI_THINKER
-
 #include "camera_pins.h"
 
-const char* ssid = "-----------"; //Wifi Name SSID
-const char* password = "----------"; //WIFI Password
+// ===== Relay config =====
+#define Relay 33            // Use GPIO33 for relay IN
+// Most relay modules are Active-LOW (IN=LOW -> ON)
+// If your relay is Active-HIGH, swap RELAY_ON / RELAY_OFF.
+#define RELAY_ON  LOW
+#define RELAY_OFF HIGH
+
+// ===== Wi-Fi =====
+const char* ssid     = "-----------"; // WiFi SSID
+const char* password = "----------";  // WiFi Password
 
 void startCameraServer();
 
-boolean matchFace = false;
-boolean activateRelay = false;
-long prevMillis=0;
-int interval = 5000;
+// Set to true from app_httpd.cpp when a face is recognized
+volatile bool matchFace = false;
+bool activateRelay = false;
+unsigned long prevMillis = 0;
+const unsigned long interval = 5000UL; // Relay ON duration (ms)
 
 void setup() {
-  pinMode(Relay,OUTPUT);
- 
-  digitalWrite(Relay,LOW);
-  
-  
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, RELAY_OFF); // relay off by default
+
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
 
+  // ===== Camera config =====
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
   config.pin_sscb_sda = SIOD_GPIO_NUM;
   config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
+
+  if (psramFound()) {
+    config.frame_size   = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
-    config.fb_count = 2;
+    config.fb_count     = 2;
   } else {
-    config.frame_size = FRAMESIZE_SVGA;
+    config.frame_size   = FRAMESIZE_SVGA;
     config.jpeg_quality = 12;
-    config.fb_count = 1;
+    config.fb_count     = 1;
   }
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
-  // camera init
+  // Initialize camera
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
+    Serial.printf("Camera init failed with error 0x%x\n", err);
+    while (true) delay(1000);
   }
 
-  sensor_t * s = esp_camera_sensor_get();
-  //initial sensors are flipped vertically and colors are a bit saturated
+  // Adjust sensor settings
+  sensor_t* s = esp_camera_sensor_get();
   if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);//flip it back
-    s->set_brightness(s, 1);//up the blightness just a bit
-    s->set_saturation(s, -2);//lower the saturation
+    s->set_vflip(s, 1);
+    s->set_brightness(s, 1);
+    s->set_saturation(s, -2);
   }
-  //drop down frame size for higher initial frame rate
-  s->set_framesize(s, FRAMESIZE_QVGA);
+  s->set_framesize(s, FRAMESIZE_QVGA); // faster FPS
 
-#if defined(CAMERA_MODEL_M5STACK_WIDE)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
+  // Wi-Fi connection
   WiFi.begin(ssid, password);
-
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("\nWiFi connected");
 
+  // Start camera web server
   startCameraServer();
 
   Serial.print("Camera Ready! Use 'http://");
@@ -112,18 +99,17 @@ void setup() {
 }
 
 void loop() {
-  if(matchFace==true && activateRelay==false)
-  {
-    activateRelay=true;
-    digitalWrite(Relay,HIGH);
-   
-    prevMillis=millis();
-    }
-    if (activateRelay == true && millis()-prevMillis > interval)
-    {
-      activateRelay=false;
-      matchFace=false;
-      digitalWrite(Relay,LOW);
-      
-      }
+  // On face match, turn relay ON for 'interval' ms
+  if (matchFace && !activateRelay) {
+    activateRelay = true;
+    digitalWrite(Relay, RELAY_ON);
+    prevMillis = millis();
+  }
+
+  // After timeout, turn relay OFF and reset flags
+  if (activateRelay && (millis() - prevMillis > interval)) {
+    activateRelay = false;
+    matchFace = false;
+    digitalWrite(Relay, RELAY_OFF);
+  }
 }
